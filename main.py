ca9c175fdd2d738ba1a69d8a356e1611fa980a0d
Nayssi <yassin.hamaoui@polytechnique.edu>
2018-05-08 02:33:41 +0200

    données prêtes

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ import pandas as pd
 
 ##
 
+#change racine
 racine="/Users/yassinhamaoui/RL-crypto/"
 fenetre=50
 
+#get the data
 data=pd.read_csv(racine+"XETHZEUR_complete_candle_15min.csv")
 
+
+#Reorganize the data
 price=data[["candle_15min","P_close"]]
 price=price.rename(index=str,columns={"P_close":"eth"})
-price["euros"]=1
 price=price.set_index("candle_15min")
-price=price[["euros","eth"]]
+data=data.set_index(price.index)
 price["eth"]=price["eth"].pct_change()+1
-y=price
+for i in range(fenetre):
+price["t-{}".format(i)]=data.P_close.shift(i)
+
+
 
 
 
